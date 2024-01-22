0d94a1fb74f406e8e93bf3c149dea9a7feba3126
cvisbal0724 <cvisbal0724@gmail.com>
2024-01-22 14:19:40 -0500

    config

---
+++ models-carrier.d.ts
@@ -0,0 +1,42 @@
+import { CarrierStatusEnum } from './';
+export declare class TransportLocationModel {
+    id: string;
+    trasnport: TransportModel;
+    shortAddress: string;
+    longAddress: string;
+    lat?: number;
+    lng?: number;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+export declare class TransportModel {
+    id: string;
+    userId: string;
+    userFullName: string;
+    status: CarrierStatusEnum;
+    initialRate: number;
+    rate: number;
+    carrierId: string;
+    carrierFullName?: string;
+    paymentMethod: string;
+    lat?: number;
+    lng?: number;
+    shortAddress: string;
+    longAddress: string;
+    comments?: string;
+    trasnportLocations: TransportLocationModel[];
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+export declare class TransportQualificationModel {
+    id: string;
+    userId: string;
+    userFullName: string;
+    carrierId: string;
+    carrierFullName: string;
+    qualification: number;
+    comment?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+export {};
--- models-security.d.ts
+++ models-security.d.ts
@@ -1,46 +1,46 @@
 import { LANGUAGE, PROFILE, ROLETYPE, VEHICLETYPE } from './';
 export declare class VehicleModel {
-    id?: string;
-    userProfile?: UserProfileModel;
-    color?: string;
-    plate?: string;
-    model?: string;
-    brand?: string;
-    type?: VEHICLETYPE;
-    createdAt?: Date;
-    updatedAt?: Date;
+    id: string;
+    userProfile: UserProfileModel;
+    color: string;
+    plate: string;
+    model: string;
+    brand: string;
+    type: VEHICLETYPE;
+    createdAt: Date;
+    updatedAt: Date;
 }
 export declare class UserProfileModel {
-    id?: string;
-    profile?: PROFILE;
-    user?: UserModel;
-    isActive?: boolean;
-    vehicles?: VehicleModel[];
-    createdAt?: Date;
-    updatedAt?: Date;
+    id: string;
+    profile: PROFILE;
+    user: UserModel;
+    isActive: boolean;
+    vehicles: VehicleModel[];
+    createdAt: Date;
+    updatedAt: Date;
 }
 export declare class UserModel {
-    id?: string;
-    firstName?: string;
-    lastName?: string;
+    id: string;
+    firstName: string;
+    lastName: string;
     email?: string;
-    countryCode?: string;
-    cellPhone?: string;
+    countryCode: string;
+    cellPhone: string;
     photo?: string;
-    isActive?: boolean;
-    role?: ROLETYPE;
+    isActive: boolean;
+    role: ROLETYPE;
     language?: LANGUAGE;
-    userProfiles?: UserProfileModel[];
-    createdAt?: Date;
-    updatedAt?: Date;
+    userProfiles: UserProfileModel[];
+    createdAt: Date;
+    updatedAt: Date;
 }
 export declare class CodeModel {
-    id?: string;
-    cellPhone?: string;
-    code?: string;
-    isActive?: boolean;
-    expiresIn?: number;
-    createdAt?: Date;
-    updatedAt?: Date;
+    id: string;
+    cellPhone: string;
+    code: string;
+    isActive: boolean;
+    expiresIn: number;
+    createdAt: Date;
+    updatedAt: Date;
 }
 export {};
