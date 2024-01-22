aa7151dd8390f74922642f9ed9c4a7ffffa51661
cvisbal0724 <cvisbal0724@gmail.com>
2024-01-22 16:11:59 -0500

    config

--- models-carrier.d.ts
+++ models-carrier.d.ts
@@ -1,40 +1,40 @@
-import { CarrierStatusEnum } from './';
+import { CarrierStatusEnum, PaymentMethodEnum } from './';
 export declare class TransportLocationModel {
-    id: string;
-    trasnport: TransportModel;
-    shortAddress: string;
-    longAddress: string;
+    id?: string;
+    trasnport?: TransportModel;
+    shortAddress?: string;
+    longAddress?: string;
     lat?: number;
     lng?: number;
     createdAt?: Date;
     updatedAt?: Date;
 }
 export declare class TransportModel {
-    id: string;
-    userId: string;
-    userFullName: string;
-    status: CarrierStatusEnum;
-    initialRate: number;
-    rate: number;
-    carrierId: string;
+    id?: string;
+    userId?: string;
+    userFullName?: string;
+    status?: CarrierStatusEnum;
+    initialRate?: number;
+    rate?: number;
+    carrierId?: string;
     carrierFullName?: string;
-    paymentMethod: string;
+    paymentMethod?: PaymentMethodEnum;
     lat?: number;
     lng?: number;
-    shortAddress: string;
-    longAddress: string;
+    shortAddress?: string;
+    longAddress?: string;
     comments?: string;
-    trasnportLocations: TransportLocationModel[];
+    trasnportLocations?: TransportLocationModel[];
     createdAt?: Date;
     updatedAt?: Date;
 }
 export declare class TransportQualificationModel {
-    id: string;
-    userId: string;
-    userFullName: string;
-    carrierId: string;
-    carrierFullName: string;
-    qualification: number;
+    id?: string;
+    userId?: string;
+    userFullName?: string;
+    carrierId?: string;
+    carrierFullName?: string;
+    qualification?: number;
     comment?: string;
     createdAt?: Date;
     updatedAt?: Date;
